12dc5626edc7684ace9796fca137132db789eb2c
Mazayw <mazayw@gmail.com>
2022-04-24 02:22:15 +0300

    fix: disable debug

--- SHELTER/assets/js/global.js
+++ SHELTER/assets/js/global.js
@@ -1,7 +1,7 @@
 const JSON_URL = 'https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/tasks/markups/level-2/shelter/pets.json';
 const frameWidth = window.innerWidth;
 console.log(`Window width: ${frameWidth}`);
-debug = true;
+debug = false;
 
 function debugInfo(text, data) {
     if (debug) {
