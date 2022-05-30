79028ec4c532299b3ff628b1df2ce59600b9d644
Mazayw <mazayw@gmail.com>
2022-05-30 13:10:18 +0300

    Revert "Updates"
    This reverts commit d1227549e4bef74b3727161cafdb9d2e7dffc67a.

---
+++ SHELTER/assets/js/global.js
@@ -0,0 +1,14 @@
+const JSON_URL = 'https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/tasks/markups/level-2/shelter/pets.json';
+const frameWidth = window.innerWidth;
+const debug = false;
+debugInfo('Window width: ', frameWidth);
+
+
+function debugInfo(text, data) {
+    if (debug) {
+        console.log(text, data);
+    }
+}
+
+
+
